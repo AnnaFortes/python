fe91de287cc00eb39c757a7172814684cf6e4b37
Ana Fortes <miah.fortes@gmail.com>
2025-01-15 13:33:46 -0300

    aulas ok

---
+++ aulas/aula03.2.py
@@ -0,0 +1,5 @@
+frase = 'Curso em Vídeo Python'
+print(frase.find('Vídeo')) #saida: 9
+print(frase.find('video')) #saida -1
+print(frase.lower().find('vídeo')) #saida: True
+print('Curso' in frase) #saida: True
---
+++ aulas/aula03.py
@@ -0,0 +1,11 @@
+frase = 'Curso em vídeo Python'
+print('\n', frase[3])
+print(frase[3:13])
+print(frase[13:])
+print(frase[1:15:2])
+print(frase[2::2])
+print(frase[::3])
+
+print('Oi')
+
+print('''A tecnologia tem avançado a passos largos, transformando a maneira como vivemos e interagimos com o mundo ao nosso redor. Desde o surgimento dos primeiros computadores, até as poderosas máquinas de hoje, a evolução tem sido impressionante. Com o aumento da inteligência artificial e do aprendizado de máquina, as possibilidades parecem ilimitadas. No entanto, com todo esse progresso, também surgem questões sobre privacidade, segurança e a ética no uso dessas tecnologias. Cabe a nós, como sociedade, encontrar um equilíbrio entre inovação e responsabilidade.''')
