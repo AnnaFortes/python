3edc15d2778b4e28f467c14e58edd293e10e7b85
Ana Fortes <miah.fortes@gmail.com>
2025-01-31 15:05:40 -0300

    desafios ok

--- desafiosMundo2/desafio29.py
+++ desafiosMundo2/desafio29.py
@@ -1,1 +1,23 @@
-#ler varios numeros inteiros, o programa so vai parar quando o usuario digitar o valor 999, qual é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles(desconsiderando o flag(flag é o número que fez o programa parar))
+cont = 0
+soma = 0
+num = 0
+
+while num != 999:
+    num = int(input('Digite um valor [999 para sair]: '))   
+    
+    cont += 1
+    soma += num
+
+print(f'Ao todo tiveram {cont} números digitados e a soma entre eles é de {soma - 999}')
+
+if num == 999:
+        print('ACABOU!')
+    
+    
+    
+
+        
+
+
+
+
--- desafiosMundo2/desafio30.py
+++ desafiosMundo2/desafio30.py
@@ -1,1 +1,25 @@
-#ler varios numeros inteiros, no final mostre a media entre tds os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuario se ele quer ou não continuar a digitar valores
+continuar = 'S'
+cont = 0
+soma = 0
+maior = None
+menor = None
+
+while continuar in 'Ss': 
+    num = int(input('Digite um número: '))
+
+    cont += 1    
+    soma += num
+
+    if maior is None or num > maior:
+        maior = num
+    if menor is None or num < menor:
+        menor = num
+
+    continuar = str(input('Gostaria de continuar? [S/N]: ')).strip().upper()
+
+media = soma / cont
+    
+print(f'A média entre todos os números é de {media}')
+print(f'O maior número foi {maior}')
+print(f'O menor número foi {menor}')
+    
