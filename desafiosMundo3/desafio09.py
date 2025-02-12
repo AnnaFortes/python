1376027717eb9df90e3a9baf4d45d432f31c8fe8
Ana Fortes <miah.fortes@gmail.com>
2025-02-12 12:21:38 -0300

    atualizações

--- desafiosMundo3/desafio09.py
+++ desafiosMundo3/desafio09.py
@@ -3,17 +3,19 @@ valores = []
 for c in range(5): 
     num = int(input('Digite um valor: '))
 
-    if not valores or num > valores[-1]: 
+    if c == 0 or num > valores[-1]: 
         valores.append(num) 
-        print('Adicionado no final da lista...')
+        print('Adicionado ao final da lista...')   
     else:
         pos = 0
 
-        while pos < len(valores) and valores[pos] < num: 
+        while pos < len(valores): 
+            if num <= valores[pos]:
+                valores.insert(pos, num) 
+                print(f'Adicionando na posição {pos} da lista...')
+                break
             pos += 1
-        valores.insert(pos, num) 
-        print(f'Adicionando na posição {pos} da lista...')
-
+                
 print('=-' * 20)
 print(f'Os valores digitados em ordem foram {valores}')
 print()
--- desafiosMundo3/desafio12.py
+++ desafiosMundo3/desafio12.py
@@ -1,2 +1,17 @@
-# crie um programa onde o usuario digita uma expressão qualquer que use parenteses. Seu aplicativo deverá analisar se a expressão passada esta com os parenteses abertos e fechados na ordem correta
+expr = str(input('Digite a expressão: ')) 
+pilha = []
 
+for simb in expr: 
+    if simb == '(': 
+        pilha.append('(') 
+    elif simb == ')':
+        if len(pilha) > 0: 
+            pilha.pop() 
+        else: 
+            pilha.append(')') 
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!') 
+else:
+    print('Sua expressão está errada!')
+print()
