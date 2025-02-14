55942dafd846e11244bcc992d4f8c82afbb3bd0f
Ana Fortes <miah.fortes@gmail.com>
2025-02-14 12:10:27 -0300

    atualizações

--- desafiosMundo3/desafio18.py
+++ desafiosMundo3/desafio18.py
@@ -1,1 +1,31 @@
-#ler nome e 2 notas de varios alunos e guarde tudo em uma lista composta, no final, mostre um boletim contendo a média de cada um e permita que o usuario possa mostrar as notas de cada aluno individualmente
+notas = []
+
+while True:
+    boletim = []
+    soma = 0
+
+    aluno = str(input('Nome: ')).strip().upper()
+    boletim.append(aluno)
+
+    for n in range(2):
+        nota = float(input(f'Nota {n + 1}: '))
+        boletim.append(nota)
+        soma += nota 
+
+    media = soma / 2
+    boletim.append(media)
+    notas.append(boletim)
+
+    continuar = str(input('Quer continuar: [S/N] ')).strip().upper()[0]
+
+    if continuar == 'N':
+        break
+print('=-' * 25)
+print(f'{"N°"} {"NOME":<15} {"MÉDIA":>6}')
+print('-'*35)
+for i, aluno in enumerate(notas):
+    print(f'{i+1:<4}{aluno[0]:<15}{aluno[-1]:>6.1f}')
+print('-'*35)
+print()
+   
+
