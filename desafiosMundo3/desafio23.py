3778c8002cdff95900720433100bea9bba8e9dd2
Ana Fortes <miah.fortes@gmail.com>
2025-02-17 09:53:21 -0300

    atualizações

--- desafiosMundo3/desafio23.py
+++ desafiosMundo3/desafio23.py
@@ -1,1 +1,39 @@
-#ler nome, sexo e idade de varias pessoas, guardando os dados de cada pessoa em um dicionario e tds os dicionarios em uma lista. No final, mostre: a)qnts pessoas foram cadastradas, b) a media de idade do grupo, c) uma lista com tds as mulheres d) uma lista com tds as pessoas com idade acima da média
+cadastro = []
+mulher = []
+pessoaAcima = []
+soma = 0
+
+while True:
+    pessoas = {}
+
+    pessoas['nome'] = str(input('Nome: '))
+    pessoas['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    pessoas['idade'] = int(input('Idade: '))
+
+    soma += pessoas['idade']
+
+    cadastro.append(pessoas)
+
+    if pessoas['sexo'] == 'F':
+        mulher.append(pessoas['nome'])
+
+    continuar = str(input('Gostaria de continuar? [S/N] ')).strip().upper()[0]
+
+    if continuar == 'N':
+        break
+
+media = soma / len(cadastro)
+
+for p in cadastro:
+    if p['idade'] > media:
+        pessoaAcima.append(p)
+
+print('=-' * 30)
+print(cadastro)
+print('=-' * 30)
+print(f'- O grupo tem {len(cadastro)} pessoas.')
+print(f'- A média de idade é de {media} anos.')
+print(f'- As mulheres cadastradas foram: {mulher}')
+print(f'- Lista de pessoas que estão acima da média: {pessoaAcima}')
+print('<< ENCERRADO >>')
+print()
--- desafiosMundo3/desafio24.py
+++ desafiosMundo3/desafio24.py
@@ -1,1 +1,23 @@
 #aprimore o desafio do jogador de futebol para que ele funcione com varios jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
+jogadores = {}
+
+while True:
+    jogadores['nome'] = str(input('Nome do jogador: '))
+    jogadores['jogos'] = int(input(f'Quantos jogos {jogadores['nome']} jogou? '))
+
+    for j in range(jogadores['jogos']):
+        jogadores['qntJogos'] = int(input(f'-> Quantos jogos na partida {j}? '))
+    
+
+    continuar = str(input('Gostaria de continuar? [S/N] ')).strip().upper()[0]
+
+    print('-' * 40)
+
+    if continuar == 'N':
+        print('=-' * 30)
+        break
+
+print(f'{"cod nome":<15}{"gols":<10}{"total":<5}')
+print('-' * 40)
+
+
