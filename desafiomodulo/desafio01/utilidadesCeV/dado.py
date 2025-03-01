c44f392b5ce3797e3e9f838db56613723b961e3c
Ana Fortes <miah.fortes@gmail.com>
2025-03-01 11:37:33 -0300

    atualização

--- desafiomodulo/desafio01/utilidadesCeV/dado.py
+++ desafiomodulo/desafio01/utilidadesCeV/dado.py
@@ -1,7 +1,7 @@
 def lerDinheiro(msg):
     while True:
-        preco = input(msg).strip().replace(',', '.') #remove espaços em branco, troca , por .
-        if preco.replace('.', '', 1).isdigit(): #verifica se é um numero(com um unico (.)ponto)
+        preco = input(msg).strip().replace(',', '.') 
+        if preco.replace('.', '', 1).isdigit(): 
             return float(preco) 
         else:
             print(f'\033[31mERRO! Digite um número!\033[m')
