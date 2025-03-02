ca511a0398c17999be61b2f98ca763079055809a
Ana Fortes <miah.fortes@gmail.com>
2025-03-02 10:02:39 -0300

    atualização

--- desafiomodulo/desafio01/teste.py
+++ desafiomodulo/desafio01/teste.py
@@ -3,3 +3,4 @@ from utilidadesCeV import moeda, dado
 preco = dado.lerDinheiro('Digite o preço: R$')
 moeda.resumo(preco, 10, 13)
 print()
+  
--- desafiomodulo/desafio01/utilidadesCeV/dado.py
+++ desafiomodulo/desafio01/utilidadesCeV/dado.py
@@ -3,6 +3,6 @@ def lerDinheiro(msg):
         preco = input(msg).strip().replace(',', '.') 
         if preco.replace('.', '', 1).isdigit(): 
             return float(preco) 
-        else:
+        else: 
             print(f'\033[31mERRO! Digite um número!\033[m')
         
